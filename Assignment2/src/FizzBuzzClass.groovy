8845de10911c7eb74914f69dc614d944677bd913
howardakumiah <hakumiah@gmail.com>
2013-10-16 11:09:14 -0400

    My first class in Groovy.

---
+++ Assignment2/src/FizzBuzzClass.groovy
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: howardakumiah
+ * Date: 10/16/13
+ * Time: 11:08 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class FizzBuzzClass {
+}
