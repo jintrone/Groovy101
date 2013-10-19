a6120fd6818365ba35d79a987b673c1e55460970
howardakumiah <hakumiah@gmail.com>
2013-10-19 15:54:46 -0400

    My first class in Groovy.

--- Assignment2/src/FizzBuzzClass.groovy
+++ Assignment2/src/FizzBuzzClass.groovy
@@ -9,7 +9,7 @@ class FizzBuzzClass {
     def factor_map = ([2:”alpha”,3:”beta”,5:”gamma”])
     static FizzBuzz (factor_map){
         for (i in 1..100){
-            println "${i%2?'':factor_map['2']}$${i%3?'':factor_map['3']}${i%5?'':factor_map[5]}"?:i
+            printl n "${i%2?'':factor_map['2']}$${i%3?'':factor_map['3']}${i%5?'':factor_map[5]}"?:i
         }
     }
 
