60da2f0a6a85d162c087a8c43165a1dbd5bc38b9
hakumiah <hakumiah@gmail.com>
2013-10-23 09:50:05 -0400

    Update FizzBuzzMultifunc.groovy

--- Assignment3/ads/FizzBuzzMultifunc.groovy
+++ Assignment3/ads/FizzBuzzMultifunc.groovy
@@ -6,13 +6,23 @@
  * To change this template use File | Settings | File Templates.
  */
 class FizzBuzzMultifunc {
+    def final_list =[]
+    def factor_map = [:]
+    def inpect_list = []
     static FizzBuzz (x,y){
-        def final_list =[]
         for (i in x..y){
             final_list.plus  ("${i%2?'':factor_map['2']}$${i%3?'':factor_map['3']}${i%5?'':factor_map[5]}"?:i)
         }
-    static Add (map_kit)
+    static add (map){
+        factor_map += [map]
     }
-
+    static remove (){
+        factor_map -= [map]
+    }
+    static inspect(){
+        for element in factor_map
+        inspect_list.plus (element)
+        return inspect_list
+    }
 
 }
