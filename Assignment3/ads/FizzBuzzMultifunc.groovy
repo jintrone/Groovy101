a77159d3a50889a31c283ebd1b9c24328b8dc086
howardakumiah <hakumiah@gmail.com>
2013-10-23 00:38:17 -0400

    My first class in Groovy.

---
+++ Assignment3/ads/FizzBuzzMultifunc.groovy
@@ -0,0 +1,18 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: howardakumiah
+ * Date: 10/23/13
+ * Time: 12:21 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class FizzBuzzMultifunc {
+    static FizzBuzz (x,y){
+        def final_list =[]
+        for (i in x..y){
+            final_list.plus  ("${i%2?'':factor_map['2']}$${i%3?'':factor_map['3']}${i%5?'':factor_map[5]}"?:i)
+        }
+    static Add (map_kit)
+    }
+
+
+}
