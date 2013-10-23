63accb1bb6a471355248ad98f58534bb261f79bf
Joshua Introne <jintrone@mit.edu>
2013-10-23 11:16:17 -0400

    Howard's correct code

--- Assignment3/ads/FizzBuzzMultifunc.groovy
+++ Assignment3/ads/FizzBuzzMultifunc.groovy
@@ -6,23 +6,53 @@
  * To change this template use File | Settings | File Templates.
  */
 class FizzBuzzMultifunc {
-    def final_list =[]
+    def final_list = []
     def factor_map = [:]
     def inpect_list = []
-    static FizzBuzz (x,y){
-        for (i in x..y){
-            final_list.plus  ("${i%2?'':factor_map['2']}$${i%3?'':factor_map['3']}${i%5?'':factor_map[5]}"?:i)
+
+
+    FizzBuzzMultifunc(Map m) {
+
+    }
+
+
+    void print(int x, int y) {
+
+        //map 1->bar;2->foo;...10;->baz
+        x..y.each { i ->
+            def final_list = []
+            factor_map.each {k,v ->
+                if (i % k == 0) final_list<<v
+
+            }
+            if (final_list.isEmpty()) println i
+            else //println final_list  [foo,bar,baz]
+
+            //final_list.plus("${i % 2 ? '' : factor_map['2']}${ i % 3 ? '' : factor_map['3'] } ${ i % 5 ? '' : factor_map[5] } " ?: i)
         }
-    static add (map){
+
+    }
+
+    void add(Map map) {
+        map.keys.each {
+
+        }
         factor_map += [map]
     }
-    static remove (){
+
+    void remove() {
         factor_map -= [map]
     }
-    static inspect(){
-        for element in factor_map
-        inspect_list.plus (element)
+
+    void inspect() {
+        for
+        element in factor_map
+        inspect_list.plus(element)
         return inspect_list
     }
+
+    static void main(String[] args) {
+    }
+}
 
 }
